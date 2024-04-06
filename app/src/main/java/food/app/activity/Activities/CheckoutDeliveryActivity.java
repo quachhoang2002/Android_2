a9c8b0fd46932c708f40a4550510d7b51e3a4494
Admin <Admin@DESKTOP-PPTN3ME>
2024-04-06 10:06:07 +0700

    cart

--- app/src/main/java/food/app/activity/Activities/CheckoutDeliveryActivity.java
+++ app/src/main/java/food/app/activity/Activities/CheckoutDeliveryActivity.java
@@ -1,5 +1,12 @@
 package food.app.activity.Activities;
 
-public class CheckoutDeliveryActivity {
+import android.os.Bundle;
 
+import food.app.activity.R;
+
+public class CheckoutDeliveryActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_delivery);
+    }
 }
--- app/src/main/java/food/app/activity/Activities/CheckoutPaymentActivity.java
+++ app/src/main/java/food/app/activity/Activities/CheckoutPaymentActivity.java
@@ -1,4 +1,12 @@
 package food.app.activity.Activities;
 
+import android.os.Bundle;
+
+import food.app.activity.R;
+
 public class CheckoutPaymentActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_order);
+    }
 }
