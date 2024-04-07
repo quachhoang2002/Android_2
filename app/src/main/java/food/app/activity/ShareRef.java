8765b300c1a8435646804de46b2683b71634e60f
quachhoang2002 <97105329+quachhoang2002@users.noreply.github.com>
2024-04-07 19:33:26 +0700

    add login

--- app/src/main/java/food/app/activity/ShareRef.java
+++ app/src/main/java/food/app/activity/ShareRef.java
@@ -3,12 +3,12 @@ package food.app.activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public class Token {
+public class ShareRef {
     private static final String PREFERENCES_FILE = "app_preferences";
     private static final String TOKEN_KEY = "token";
     private SharedPreferences sharedPreferences;
 
-    public Token(Context context) {
+    public ShareRef(Context context) {
         sharedPreferences = context.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
     }
 
@@ -20,5 +20,20 @@ public class Token {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(TOKEN_KEY, token);
         editor.apply();
+    }
+
+    public void saveUserInformation(String email, String phone) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email", email);
+        editor.putString("phone", phone);
+        editor.apply();
+    }
+
+    public String getEmail() {
+        return sharedPreferences.getString("email", null);
+    }
+
+    public String getPhone() {
+        return sharedPreferences.getString("phone", null);
     }
 }
