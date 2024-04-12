163a92a14df71347b43fe66b11139a49d2c55ca9
LienHung <kozaxlh@gmail.com>
2024-04-13 01:59:14 +0700

    Profile User

---
+++ app/src/main/java/food/app/activity/Models/ResponseObject.java
@@ -0,0 +1,39 @@
+package food.app.activity.Models;
+
+import java.util.List;
+
+public class ResponseObject<T> {
+    private String status;
+    private String message;
+    private T data;
+
+    public ResponseObject(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
